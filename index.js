aaaa6e92a4fad61ace02b30cf8886531fe294b68
amit dhankhar <amitdhankhar616@gmail.com>
2020-02-03 23:56:23 +0530

    coding

--- index.js
+++ index.js
@@ -13,3 +13,45 @@ function f1(x){
     console.log(x);
 }
 f1(12);
+
+
+
+
+
+
+//cb.lk/mapReduce
+/*(document.getElementById("id")
+
+button.onclick= function fi()
+{
+    console.log("click")
+
+
+}*/
+
+console.log(1)
+console.log(2)
+     setTimeout(() => {
+
+    Console.log("after one secnd")
+},1000)
+console.log(3)
+for(let i=0;i<5;i++)
+{
+    console.log()
+}
+
+console.log("done")
+
+/*
+newFunction();
+function newFunction() {
+    const generatePromise = (val1, n) => new Promise(resolve);
+    setTimeout(() => {
+    }, timeout);
+    const generatePromiseHOF = (bhadak);
+}
+*/
+
+
+
---
+++ object.js
@@ -0,0 +1,7 @@
+obj={
+
+    var x=4;
+    var y=5;
+   let z=6;
+}
+console.log(z);
