e8dc8ae964c06dff39c02625038ff843d7baf7bf
amit dhankhar <amitdhankhar616@gmail.com>
2020-02-08 23:48:11 +0530

    nochnge

--- createserver.js
+++ createserver.js
@@ -2,6 +2,6 @@ var express=require('express')
 var app=express()
 app.get('/a',(req,res)=>res.send("myfirst response"))
 
-app.get(('/',(req,res)=>res.send("error 404"))
+//app.get(('/',(req,res)=>res.send("error 404"))
 
 app.listen(3000)
