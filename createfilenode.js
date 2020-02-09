49494677192271aa929524febdebcec62cdd2aa5
amit dhankhar <amitdhankhar616@gmail.com>
2020-02-09 23:59:57 +0530

    chnge

--- createfilenode.js
+++ createfilenode.js
@@ -4,13 +4,3 @@ fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
   console.log('Saved!');
 });
 
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-  //Open a file on the server and return its content:
-  fs.readFile('demofile1.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080);
